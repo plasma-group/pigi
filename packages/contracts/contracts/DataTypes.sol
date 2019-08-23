23af76d8d5e36879eca3a8d1ac0558f7371c9159
Karl Floersch <karl@karlfloersch.com>
2019-08-23 15:49:52 +0000

    Remove CommitmentChain & add RollupChain

--- packages/contracts/contracts/DataTypes.sol
+++ packages/contracts/contracts/DataTypes.sol
@@ -8,41 +8,4 @@ pragma experimental ABIEncoderV2;
 contract DataTypes {
 
     /*** Structs ***/
-    struct Range {
-        uint256 start;
-        uint256 end;
-    }
-
-    struct StateObject {
-        address predicateAddress;
-        bytes data;
-    }
-
-    struct StateUpdate {
-        StateObject stateObject;
-        Range range;
-        uint256 plasmaBlockNumber;
-        address depositAddress;
-    }
-
-    struct Checkpoint {
-        StateUpdate stateUpdate;
-        Range subrange;
-    }
-
-    struct Transaction {
-        address depositAddress;
-        bytes body;
-        Range range;
-    }
-
-    struct AssetTreeNode {
-        bytes32 hashValue;
-        uint256 start;
-    }
-    
-    struct StateSubtreeNode {
-        bytes32 hashValue;
-        uint128 start;
-    }
 }
