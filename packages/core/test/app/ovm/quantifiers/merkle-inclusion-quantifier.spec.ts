e6bb5956adbe42b2ddf9296aff00bd36b5a646e8
Will Meister <william.meister@consensys.net>
2019-08-29 14:58:57 -0500

    Fixing linting issues

--- packages/core/test/app/ovm/quantifiers/merkle-inclusion-quantifier.spec.ts
+++ packages/core/test/app/ovm/quantifiers/merkle-inclusion-quantifier.spec.ts
@@ -1,14 +1,12 @@
 import '../../../setup'
 
-import * as assert from "assert";
+import * as assert from 'assert'
 
-
-import {MerkleTree} from "../../../../src/types/utils";
-import {BigNumber, QuantifierResult, TWO, ZERO} from "../../../../src/types";
-import {MerkleInclusionQuantifier} from "../../../../src/app/ovm/quantifiers";
+import { MerkleTree } from '../../../../src/types/utils'
+import { BigNumber, QuantifierResult, TWO, ZERO } from '../../../../src/types'
+import { MerkleInclusionQuantifier } from '../../../../src/app/ovm/quantifiers'
 
 class MockedMerkleTree implements MerkleTree {
-
   private rootHash: Buffer
   private readonly leaves: {} = {}
 
@@ -24,17 +22,18 @@ class MockedMerkleTree implements MerkleTree {
     if (!!rootHash && (!this.rootHash || !this.rootHash.equals(rootHash))) {
       return undefined
     }
-    return leafKey.toString() in this.leaves ? this.leaves[leafKey.toString()] : undefined
+    return leafKey.toString() in this.leaves
+      ? this.leaves[leafKey.toString()]
+      : undefined
   }
 
   public async getRootHash(): Promise<Buffer> {
-    return this.rootHash;
+    return this.rootHash
   }
 
   public async update(key: BigNumber, value: Buffer): Promise<boolean> {
-    return undefined;
+    return undefined
   }
-
 }
 
 describe('MerkleTreeQuantifier', () => {
@@ -50,12 +49,21 @@ describe('MerkleTreeQuantifier', () => {
       const quantifier = new MerkleInclusionQuantifier(merkleTree)
       const result: QuantifierResult = await quantifier.getAllQuantified({
         key,
-        root: Buffer.from("some root")
+        root: Buffer.from('some root'),
       })
 
-      assert(result.results.length === 1, 'There should only be one result that is undefined.')
-      assert(result.results[0] === undefined, 'There should only be one result that is undefined.')
-      assert(!result.allResultsQuantified, 'All results should not be quantified if we are unable to find a leaf node in the tree.')
+      assert(
+        result.results.length === 1,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        result.results[0] === undefined,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        !result.allResultsQuantified,
+        'All results should not be quantified if we are unable to find a leaf node in the tree.'
+      )
     })
 
     it('returns undefined on populated tree with a root that does not match', async () => {
@@ -65,64 +73,100 @@ describe('MerkleTreeQuantifier', () => {
 
       const result: QuantifierResult = await quantifier.getAllQuantified({
         key,
-        root: Buffer.from("some root")
+        root: Buffer.from('some root'),
       })
 
-      assert(result.results.length === 1, 'There should only be one result that is undefined.')
-      assert(result.results[0] === undefined, 'There should only be one result that is undefined.')
-      assert(!result.allResultsQuantified, 'All results should not be quantified if we are unable to find a leaf node in the tree.')
+      assert(
+        result.results.length === 1,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        result.results[0] === undefined,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        !result.allResultsQuantified,
+        'All results should not be quantified if we are unable to find a leaf node in the tree.'
+      )
     })
 
     it('returns undefined on empty tree with a root that matches', async () => {
       const quantifier = new MerkleInclusionQuantifier(merkleTree)
-      const root: Buffer = Buffer.from("some root")
+      const root: Buffer = Buffer.from('some root')
 
       merkleTree.setRootHash(root)
 
       const result: QuantifierResult = await quantifier.getAllQuantified({
         key,
-        root
+        root,
       })
 
-      assert(result.results.length === 1, 'There should only be one result that is undefined.')
-      assert(result.results[0] === undefined, 'There should only be one result that is undefined.')
-      assert(!result.allResultsQuantified, 'All results should not be quantified if we are unable to find a leaf node in the tree.')
+      assert(
+        result.results.length === 1,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        result.results[0] === undefined,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        !result.allResultsQuantified,
+        'All results should not be quantified if we are unable to find a leaf node in the tree.'
+      )
     })
 
     it('returns value on populated tree tree with a root that matches', async () => {
       const quantifier = new MerkleInclusionQuantifier(merkleTree)
-      const root: Buffer = Buffer.from("some root")
-      const value: Buffer = Buffer.from("VALUE")
+      const root: Buffer = Buffer.from('some root')
+      const value: Buffer = Buffer.from('VALUE')
 
       merkleTree.setRootHash(root)
       merkleTree.setLeaf(key, value)
 
       const result: QuantifierResult = await quantifier.getAllQuantified({
         key,
-        root
+        root,
       })
 
-      assert(result.results.length === 1, 'There should only be one result that is the value.')
-      assert(result.results[0].equals(value) , `Result value [${result.results[0].toString()}] found when expecting [${value.toString()}]`)
-      assert(result.allResultsQuantified, 'All results should be quantified if we are able to find the leaf node in the tree.')
+      assert(
+        result.results.length === 1,
+        'There should only be one result that is the value.'
+      )
+      assert(
+        result.results[0].equals(value),
+        `Result value [${result.results[0].toString()}] found when expecting [${value.toString()}]`
+      )
+      assert(
+        result.allResultsQuantified,
+        'All results should be quantified if we are able to find the leaf node in the tree.'
+      )
     })
 
     it('returns value on populated tree tree with a root that matches but mismatched key', async () => {
       const quantifier = new MerkleInclusionQuantifier(merkleTree)
-      const root: Buffer = Buffer.from("some root")
-      const value: Buffer = Buffer.from("VALUE")
+      const root: Buffer = Buffer.from('some root')
+      const value: Buffer = Buffer.from('VALUE')
 
       merkleTree.setRootHash(root)
       merkleTree.setLeaf(key, value)
 
       const result: QuantifierResult = await quantifier.getAllQuantified({
         key: TWO,
-        root
+        root,
       })
 
-      assert(result.results.length === 1, 'There should only be one result that is undefined.')
-      assert(result.results[0] === undefined, 'There should only be one result that is undefined.')
-      assert(!result.allResultsQuantified, 'All results should not be quantified if we are unable to find a leaf node in the tree.')
+      assert(
+        result.results.length === 1,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        result.results[0] === undefined,
+        'There should only be one result that is undefined.'
+      )
+      assert(
+        !result.allResultsQuantified,
+        'All results should not be quantified if we are unable to find a leaf node in the tree.'
+      )
     })
   })
 })
