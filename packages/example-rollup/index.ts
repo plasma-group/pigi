b3882d0d84d356b884b2f74e2ee66ade6f49a206
Karl Floersch <karl@karlfloersch.com>
2019-09-02 20:04:47 +0000

    Start adding client script

--- packages/example-rollup/index.ts
+++ packages/example-rollup/index.ts
@@ -1,4 +1,41 @@
-export * from './src'
+// /* Imports */
+// import { BaseDB } from '@pigi/core'
+// import MemDown from 'memdown'
+// import {
+//   State,
+//   UNISWAP_ADDRESS,
+//   AGGREGATOR_ADDRESS,
+//   UnipigWallet,
+// } from '@pigi/wallet'
 
-// tslint:disable-next-line
-console.log('yo what is gucci')
+// /* Global declarations */
+// declare function updateAccountAddress(address: string): void
+// declare function updateBalances(balances: object): void
+
+// const newMemDown = new MemDown('ovm') as any
+// console.log(newMemDown)
+// // const client = new SimpleClient('http://167.99.234.5:3000')
+// console.log(BaseDB)
+// // const test = new BaseDB(newMemDown)
+// // console.log(test)
+
+// /* Body */
+// // const db = new BaseDB(new MemDown('ovm') as any)
+// // const unipigWallet = new UnipigWallet(db)
+// // // Now create a wallet account
+// // const accountAddress = 'mocked account'
+
+// // console.log(new SimpleClient('http://167.99.234.5:3000'))
+
+// // Connect to the mock aggregator
+// // unipigWallet.rollup.connect(new SimpleClient('http://167.99.234.5:3000'))
+
+// // // tslint:disable-next-line
+// // updateAccountAddress(accountAddress)
+
+// // async function fetchBalanceUpdate() {
+// //   console.log('test im here')
+// //   const response = await unipigWallet.getBalances(UNISWAP_ADDRESS)
+// //   updateBalances(response)
+// // }
+// // fetchBalanceUpdate()
