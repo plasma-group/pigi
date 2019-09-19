f7dd36eeea2220c1ab5d0a993f24fa5bd8b1b61b
Will Meister <william.meister@consensys.net>
2019-09-19 10:24:52 -0500

    UNISWAP_ADDRESS => NULL_ADDRESS

--- packages/wallet/src/utils.ts
+++ packages/wallet/src/utils.ts
@@ -1,9 +1,9 @@
 import { Address, TokenType, RollupTransaction } from './index'
-import { SignatureProvider, SignatureVerifier } from '@pigi/core'
+import { NULL_ADDRESS, SignatureProvider, SignatureVerifier } from '@pigi/core'
 
 /* Constants */
 export const AGGREGATOR_ADDRESS = '0xAc001762c6424F4959852A516368DBf970C835a7'
-export const UNISWAP_ADDRESS = '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF'
+export const UNISWAP_ADDRESS = NULL_ADDRESS
 export const UNI_TOKEN_TYPE = 0
 export const PIGI_TOKEN_TYPE = 1
 
